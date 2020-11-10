d70f3d50acf8f3f73db3fb0002f6af4183d532c9
Rodney Ditchfield <rodney.ditch@hotmail.com>
2020-11-10 16:30:45 +1100

    removed card details from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_040626) do
+ActiveRecord::Schema.define(version: 2020_11_10_052135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,12 +64,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_040626) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "stripe_id"
-    t.string "card_brand"
-    t.string "card_last4"
-    t.string "card_exp_month"
-    t.string "card_exp_year"
-    t.string "expires_at"
     t.boolean "admin", default: false
     t.boolean "client", default: true
     t.string "name"
