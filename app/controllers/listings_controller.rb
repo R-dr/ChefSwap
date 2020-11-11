3aba2fa694bf4198a207cfe17b6c11800777ffe9
Rodney Ditchfield <rodney.ditch@hotmail.com>
2020-11-11 15:44:34 +1100

    cleaned up listings controller json garbage

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,13 +5,11 @@ class ListingsController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_listing, only: %i[new create]
   # GET /listings
-  # GET /listings.json
   def index
     @listings = Listing.all
   end
 
   # GET /listings/1
-  # GET /listings/1.json
   def show; end
 
   def home
@@ -27,44 +25,32 @@ class ListingsController < ApplicationController
   def edit; end
 
   # POST /listings
-  # POST /listings.json
   def create
     @listing = Listing.create(listing_params)
     @listing.user = current_user
 
-    respond_to do |format|
+    
       if @listing.save
-        format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+         redirect_to @listing, notice: 'Listing was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @listing.errors, status: :unprocessable_entity }
+         render :new
       end
-    end
+    
   end
 
   # PATCH/PUT /listings/1
-  # PATCH/PUT /listings/1.json
   def update
-    respond_to do |format|
       if @listing.update(listing_params)
-        format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+         redirect_to @listing, notice: 'Listing was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @listing.errors, status: :unprocessable_entity }
+        f render :edit
       end
-    end
   end
 
   # DELETE /listings/1
-  # DELETE /listings/1.json
   def destroy
     @listing.destroy
-    respond_to do |format|
       format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
