ff20ab14118a999c5bea78e7fee4e6ef0ab3b60d
Rodney Ditchfield <rodney.ditch@hotmail.com>
2020-11-16 18:19:19 +1100

    attempt at integratnig flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  add_flash_types :error, :success
 
+  layout :layout_for_resource
   protected
+
+  def layout_for_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name is_chef avatar])
